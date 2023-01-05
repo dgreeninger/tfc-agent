9db089439b5c0669ab1db43e2796f5fa4f011b12
Daniel Greeninger <daniel.greeninger@hashicorp.com>
2023-01-05 13:48:50 -0800

    try again

--- tfc-agent-ecs/producer/terraform.tf
+++ tfc-agent-ecs/producer/terraform.tf
@@ -1,9 +1,3 @@
-variable "is_aws_profile_used" {
-  description = "Indicate if you are using an aws-cli profile or not (for local deployment only, must be equal to false on Jenkins)"
-  type        = bool
-  default     = false
-}
-
 terraform {
   required_version = ">= 1.0.0"
   aws = {
@@ -12,7 +6,3 @@ terraform {
   }
 }
 
-provider "aws" {
-  profile = var.is_aws_profile_used ? var.aws_cli_profile : null
-  region  = var.region
-}
