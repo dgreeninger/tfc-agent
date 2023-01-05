9bcb15f0b742aa63693cdf2d932f1c60703f217f
Daniel Greeninger <daniel.greeninger@hashicorp.com>
2023-01-05 13:37:54 -0800

    upgrading auth

--- tfc-agent-ecs/producer/terraform.tf
+++ tfc-agent-ecs/producer/terraform.tf
@@ -1,8 +1,18 @@
+variable "is_aws_profile_used" {
+  description = "Indicate if you are using an aws-cli profile or not (for local deployment only, must be equal to false on Jenkins)"
+  type        = bool
+  default     = false
+}
+
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.48.0"
-    }
+  required_version = ">= 1.0.0"
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 4.48.0"
   }
+}
+
+provider "aws" {
+  profile = var.is_aws_profile_used ? var.aws_cli_profile : null
+  region  = var.region
 }
